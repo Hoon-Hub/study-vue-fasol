4deb71e682c07e972acc438a0ec2eb6bf285870d
Hoon-Hub <gwi01304@naver.com>
2022-11-15 14:37:36 +0900

    03 added

---
+++ 03-nuxt-ts-counter/components/HeaderComponents.vue
@@ -0,0 +1,10 @@
+<template lang="">
+  <div>
+    <h1>dhel</h1>
+    <p>{{ dataFlose }}</p>
+  </div>
+</template>
+<script lang="ts" setup>
+const dataFlose = 'hello TS'
+</script>
+<style lang=""></style>
---
+++ 03-nuxt-ts-counter/pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <Header-components />
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage',
+  components: {},
+})
+</script>
